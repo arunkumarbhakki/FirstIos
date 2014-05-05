0ff017681c8de23b10aa637720ef245fb3335ced
arunkumarbhakki <arunkumar06may89@gmail.com>
2014-05-05 13:54:58 +0530

    First Commit

---
+++ TestappTests/TestappTests.h
@@ -0,0 +1,13 @@
+//
+//  TestappTests.h
+//  TestappTests
+//
+//  Created by AC INFOTECH INC on 5/5/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestappTests : SenTestCase
+
+@end
---
+++ TestappTests/TestappTests.m
@@ -0,0 +1,32 @@
+//
+//  TestappTests.m
+//  TestappTests
+//
+//  Created by AC INFOTECH INC on 5/5/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestappTests.h"
+
+@implementation TestappTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestappTests");
+}
+
+@end
